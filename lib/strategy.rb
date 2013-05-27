2838a5978c5040153c3e4be5bb10b20855de2fd1
Todd Pickell <toddpickell@MacMini.local>
2013-05-27 08:48:28 -0500

    all tests pass with basic strategy pattern implementation

--- lib/strategy.rb
+++ lib/strategy.rb
@@ -1,20 +1,32 @@
 class ContextClass
-  attr_accessor :strategy, :text
-
-  def initialize(strategy = nil)
-    @strategy = strategy
-  end
+  attr_reader :strategy, :text
 
   def text=(text)
     @text = text
+  end
+
+  def strategy=(strategy)
+    @strategy = strategy
+  end
+
+  def output
+    @strategy.call(self)
   end
 
 end
 
 class StrategyOne
 
+  def call(context)
+    context.text.reverse
+  end
+
 end
 
 class StrategyTwo
+
+  def call(context)
+    context.text.upcase
+  end
 
 end
--- spec/strategy_spec.rb
+++ spec/strategy_spec.rb
@@ -5,25 +5,78 @@ describe ContextClass do
   before(:each) do
     @strategy = mock "Strategy"
     @text = "This is my test string of text."
-    @context = ContextClass.new(@strategy)
+    @context = ContextClass.new
   end
 
-  it "should store a strategy on initialization" do
-    @context.strategy.should be @strategy
+  describe "#text=" do
+    it "should be able to store a string of text" do
+      @context.text = @text
+      @context.text.should == @text
+    end
   end
 
-  it "should be able to store a string of text" do
-    @context.text = @text
-    @context.text.should == @text
+  describe "#strategy=" do
+    it "should be able to store a strategy" do
+      @context.strategy = @strategy
+      @context.strategy.should be @strategy
+    end
   end
-  
+
+  describe "#output" do
+    it "should output the text according to the strategy" do
+      @text = "This is my test string of text."
+      @strategyOne = StrategyOne.new
+      @strategyTwo = StrategyTwo.new
+      @context.text = @text
+      @context.strategy = @strategyOne
+      @context.output.should == @text.reverse
+      @context.strategy = @strategyTwo
+      @context.output.should == @text.upcase
+    end
+  end
 
 end
 
 describe StrategyOne do
 
+  before(:each) do
+    @strategy = StrategyOne.new
+    @text = "This is my test string of text."
+    @context = ContextClass.new
+    @context.text = @text
+    @context.strategy = @strategy
+  end
+
+  it "should respond to call" do
+    @strategy.should respond_to :call
+  end
+
+  describe "#call" do
+    it "should return formatted text" do
+      @strategy.call(@context).should == @text.reverse
+    end
+  end
+
 end
 
 describe StrategyTwo do
+
+  before(:each) do
+    @strategy = StrategyTwo.new
+    @text = "This is my test string of text."
+    @context = ContextClass.new
+    @context.text = @text
+    @context.strategy = @strategy
+  end
+
+  it "should respond to call" do
+    @strategy.should respond_to :call
+  end
+
+  describe "#call" do
+    it "should return formatted text" do
+      @strategy.call(@context).should == @text.upcase
+    end
+  end
 
 end
